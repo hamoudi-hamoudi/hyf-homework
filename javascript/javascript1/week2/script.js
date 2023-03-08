90c29af08c442aab85abde8557f41f8668b5deab
hamoudi-hamoudi <mr-hamoudi@outlook.com>
2023-03-08 16:26:27 +0100

    feedback correction

--- javascript/javascript1/week2/script.js
+++ javascript/javascript1/week2/script.js
@@ -1,29 +1,31 @@
 // Flight booking fullname function
 
 function getFullname(firstname, surname, gender, useFormalName) {
-  if (gender === "man" && useFormalName === true) {
-    console.log("Your client full name is lord " + firstname + " " + surname);
-  } else if (gender === "woman" && useFormalName === true) {
-    console.log(
-      "Your client full name is princess " + firstname + " " + surname
-    );
+  if (firstname !== "" && surname !== "") {
+    if (gender === "man" && useFormalName === "y") {
+      return "Your client full name is lord " + firstname + " " + surname;
+    } else if (gender === "woman" && useFormalName === "y") {
+      return "Your client full name is princess " + firstname + " " + surname;
+    } else {
+      return "Your client full name " + firstname + " " + surname;
+    }
   } else {
-    console.log("Your client full name " + firstname + " " + surname);
+    return "please enter a valid name";
   }
 }
-let useFormalName = true;
-getFullname(
+let flightClient = getFullname(
   prompt("please enter your first name"),
   prompt("please enter your last name"),
   prompt("what is your gender ?"),
-  true
+  prompt("would you like to use a formal name ? (Y/N)")
 );
+console.log(flightClient);
 
 // Event application
 
-let today = new Date();
 let dayToEvent = +prompt("How Many Days Left To The Event ?");
 function getEventWeekday(days) {
+  let today = new Date();
   today.setDate(today.getDate() + days);
   return today.toLocaleString("default", { weekday: "long" });
 }
@@ -31,22 +33,21 @@ console.log("The event in happening on " + getEventWeekday(dayToEvent));
 
 // Weather wear
 
-function weatherWear(temperature) {
+function checkWeatherCloths(temperature) {
   if (temperature <= 5) {
-    console.log("Stay home its freezing outside and wear your blanket");
+    return "Stay home its freezing outside and wear your blanket";
   } else if (temperature > 5 && temperature < 15) {
-    console.log("Wear warm clothes most important dont forget your jacket");
+    return "Wear warm clothes most important dont forget your jacket";
   } else if (temperature >= 15 && temperature <= 29) {
-    console.log("its a beach day wear your light clothes");
+    return "its a beach day wear your light clothes";
   } else if (temperature >= 30 && temperature < 45) {
-    console.log(
-      "the heat can hurt you, wear light color clothes and cover your head with a hat"
-    );
+    return "the heat can hurt you, wear light color clothes and cover your head with a hat";
   } else {
-    console.log("I think you are in another planet");
+    return "I think you are in another planet";
   }
 }
-weatherWear(+prompt("what temp is it "));
+let weatherWear = checkWeatherCloths(+prompt("what temp is it "));
+console.log(weatherWear);
 
 // Student manager
 
@@ -80,3 +81,42 @@ function getNumberOfStudents() {
   console.log("there is " + class07Students.length + " students in the class");
 }
 getNumberOfStudents();
+
+// Candy helper optional
+
+function addCandy(candyType, weight) {
+  if (candyType === "Sweet") {
+    return boughtCandyPrices.push(weight * 0.5);
+  } else if (candyType === "Chocolate") {
+    return boughtCandyPrices.push(weight * 0.7);
+  } else if (candyType === "Toffee") {
+    return boughtCandyPrices.push(weight * 1.1);
+  } else if (candyType === "Chewing-gum") {
+    return boughtCandyPrices.push(weight * 0.03);
+  }
+}
+let boughtCandyPrices = [];
+addCandy("Sweet", 6);
+addCandy("Chocolate", 10);
+addCandy("Toffee", 10);
+addCandy("Toffee", 10);
+addCandy("Chewing-gum", 100);
+console.log(boughtCandyPrices);
+
+let amountToSpend = Math.random() * 100;
+
+function canBuyMoreCandy() {
+  let totalPrice = 0;
+  for (i = 0; i < boughtCandyPrices.length; i++) {
+    totalPrice = boughtCandyPrices[i] + totalPrice;
+  }
+  if (amountToSpend >= totalPrice) {
+    return true;
+  } else return false;
+}
+let resultCandy =
+  canBuyMoreCandy() === true
+    ? "You can buy more, so please do!"
+    : "Enough candy for you!";
+
+console.log(resultCandy);
