cc7d7b11d03854e5b31a7625b144d90386058546
hamoudi-hamoudi <mr-hamoudi@outlook.com>
2023-03-28 00:53:32 +0200

    Js2 week 1 Homework

---
+++ javascript/javascript2/week1/hyf-bay/main.js
@@ -0,0 +1,20 @@
+console.log("Script loaded");
+
+const products = getAvailableProducts();
+console.log(products);
+let mainTag = document.querySelector("#maintag");
+
+function renderProducts(str) {
+  for (product of str) {
+    let h2Tag = document.createElement("h2");
+    let pTag = document.createElement("p");
+    let pTag2 = document.createElement("p");
+    mainTag.appendChild(h2Tag);
+    mainTag.appendChild(pTag);
+    mainTag.appendChild(pTag2);
+    h2Tag.innerHTML = product.name;
+    pTag.innerHTML = `price : ${product.price} `;
+    pTag2.innerHTML = `Rating: ${product.rating} `;
+  }
+}
+renderProducts(products);
