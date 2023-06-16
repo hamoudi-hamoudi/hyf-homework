8fe5b8edfb644ef0ea959b8d2ee67f7788a550c4
hamoudi-hamoudi <mr-hamoudi@outlook.com>
2023-06-16 15:06:43 +0200

    nodejs week2 after review changes

--- nodejs/week2/app.js
+++ nodejs/week2/app.js
@@ -1,37 +1,60 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 const documents = require("./documents");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/search", (req, res) => {
-  try {
-    const query = req.query.q;
-    if (!query) {
-      res.status(200).json(documents);
-    } else if (!isNaN(+query)) {
-      const result = documents.filter((e) => e.id === +query);
-      result.length === 0
-        ? res.status(200).json(documents)
-        : res.status(200).json(result);
-    } else {
-      const result = documents.filter((e) =>
-        Object.values(e).some(
-          (v) =>
-            typeof v === "string" &&
-            v.toLowerCase().includes(query.toLowerCase())
-        )
-      );
-      result.length === 0
-        ? res.status(200).json(documents)
-        : res.status(200).json(result);
+app
+  .route("/search")
+  .get((req, res) => {
+    try {
+      const query = req.query.q;
+      if (query) {
+        const result = documents.filter((e) =>
+          Object.values(e).some(
+            (v) =>
+              typeof v === "string" &&
+              v.toLowerCase().includes(query.toLowerCase())
+          )
+        );
+        if (result.length > 0) res.status(200).json(result);
+      } else {
+        res.status(200).json(documents);
+      }
+    } catch (err) {
+      res.status(500).json(err);
     }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+  })
+  .post((req, res) => {
+    const query = req.query.q;
+    const body = req.body.fields;
+    try {
+      if (body && query) {
+        res
+          .status(400)
+          .json(
+            "Both 'q' query parameter and 'fields' in the request body cannot be provided together."
+          );
+      } else if (body) {
+        const fields = JSON.parse(body);
+        const result = documents.filter((e) =>
+          Object.entries(fields).every(([key, value]) => e[key] === value)
+        );
+        result.length === 0
+          ? res.status(404).json("No results with this information.")
+          : res.status(200).json(result);
+      } else if (query) {
+        res.redirect("/search?q=" + query);
+      } else {
+        res.status(200).json(documents);
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 app.get("/documents/:id", (req, res) => {
   try {
     const id = req.params.id;
@@ -39,44 +62,6 @@ app.get("/documents/:id", (req, res) => {
     result.length === 0
       ? res.status(404).json("no result with this id")
       : res.status(200).json(result);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-app.post("/search", (req, res) => {
-  try {
-    const query = req.query.q;
-    const fields = req.body.fields;
-    if (!query && !fields) {
-      res.status(200).json(documents);
-    } else if (!query && fields) {
-      const result = document.filter((e) =>
-        Object.keys(fields).every((k) => fields[k] === e[k])
-      );
-      result.length === 0
-        ? res.status(404).json("no result with this id")
-        : res.status(200).json(result);
-    } else if (query && !fields) {
-      if (!isNaN(+query)) {
-        const result = documents.filter((e) => e.id === +query);
-        result.length === 0
-          ? res.status(200).json("no result found")
-          : res.status(200).json(result);
-      }
-    } else if (query && fields) {
-      res.status(400).json("Bad request");
-    } else {
-      const result = documents.filter((e) =>
-        Object.values(e).some(
-          (v) =>
-            typeof v === "string" &&
-            v.toLowerCase().includes(query.toLowerCase())
-        )
-      );
-      result.length > 0
-        ? res.status(200).json("no result found")
-        : res.status(200).json(result);
-    }
   } catch (err) {
     res.status(500).json(err);
   }
